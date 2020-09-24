a871b0e82208a853df38c9800763ad73e589579c
NIKHIL WANI <65514201+Nikhil-Wani@users.noreply.github.com>
2020-09-24 11:08:04 +0530

    Update flasksignup.py

--- Flask/flasksignup.py
+++ Flask/flasksignup.py
@@ -14,7 +14,7 @@ mysql.init_app(app)
 
 @app.route("/")
 def main():
-    return render_template('index.html')
+    return render_template('flaskindex.html')
 
 @app.route('/showSignUp')
 def showSignUp():
